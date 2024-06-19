9257f23363b2efc1147007448c51215bf6a05c91
Aditya <b21242@students.iitmandi.ac.in>
2024-06-19 06:15:44 +0530

    seperated paths code

--- utils/task_locations.py
+++ utils/task_locations.py
@@ -12,3 +12,10 @@ class TaskLocation:
         pygame.draw.rect(screen, (255, 0, 0), (self.x - 5, self.y - 5, 10, 10))
         task_text = self.font.render(self.task, True, (0, 0, 0), (255, 255, 255))  # Set white background color
         screen.blit(task_text, (self.x + 10, self.y - 10))
+
+class Path:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = (100, 100, 100)  # Gray color for the obstacle
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
